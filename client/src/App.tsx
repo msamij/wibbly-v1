6c268be0926424be385bd4868c022c5d2845ed77
Muhammad Sami <muhammadsami290@outlook.com>
2022-10-19 15:43:00 +0500

    app component refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,4 @@
 import React from 'react';
-import BookingList from '@bookingList/BookingList';
-import Footer from '@footer/Footer';
-import Form from '@form/Form';
-import Header from '@header/Header';
-import Main from '@main/Main';
-import Message from '@message/Message';
-import NotificationList from '@notificationList/NotificationsList';
-import Overlay from '@overlay/Overlay';
-import ReviewMenu from '@reviewMenu/ReviewMenu';
 import Products from './pages/products/components/App';
 import './App.css';
 
